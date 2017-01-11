af35c1097c6e404ebacc6e2eda3f253e70ad4ed9
triki <triki@triki-HP-EliteBook-840-G3>
2017-01-11 15:30:45 +0100

    Add logger

--- cocam/src/com/trksoft/cocam/TeamStatComparator.java
+++ cocam/src/com/trksoft/cocam/TeamStatComparator.java
@@ -6,12 +6,17 @@
 package com.trksoft.cocam;
 
 import java.util.Comparator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
  * @author PSUANZES
  */
 public class TeamStatComparator implements Comparator<TeamStat> {
+    @SuppressWarnings("NonConstantFieldWithUpperCaseName")
+    private static final Logger logger
+        = LogManager.getLogger(TeamStatComparator.class);
 
     // por tipo de liga: REGular PEdro Menendez, CArreño Miranda
     // por puntos, descendente
@@ -23,6 +28,15 @@ public class TeamStatComparator implements Comparator<TeamStat> {
     // si persiste el empate habra que ver que se hace (desempate)
     @Override
     public int compare(TeamStat oneTeamStat, TeamStat otherTeamStat) {
+
+        /*
+        logger.info("Comparing Team Stats("
+            + oneTeamStat.getTeamStatKey()
+            + ") vs ("
+            + otherTeamStat.getTeamStatKey()
+            + ")"
+        );*/
+
         // por tipo de liga alfabetico
          int i = oneTeamStat.getLeagueType().compareTo(
              otherTeamStat.getLeagueType());
@@ -32,6 +46,11 @@ public class TeamStatComparator implements Comparator<TeamStat> {
         i = otherTeamStat.getPoints().compareTo(oneTeamStat.getPoints());
         if (i!=0) return i;
         
+        /*
+        logger.info("EMPATAN A PUNTOS("
+            + oneTeamStat.getPoints()
+            + ")");*/
+        
         // se busca el enfrentamineto directo, si existe
         if (oneTeamStat.getDirectMatch().
             containsKey(otherTeamStat.getTeamId())) {
@@ -40,15 +59,27 @@ public class TeamStatComparator implements Comparator<TeamStat> {
             // si existe en el primer equipo, debe existir en el segundo
             DirectMatch otherDirectMatch = otherTeamStat.getDirectMatch().get(
                 oneTeamStat.getTeamId());
+            
+            //logger.info("Comparing Direct Macth");
+            
             // solo tiene sentido si se ha jugado ida y vuelta
             // durante la primera vuelta es vacuo
             if (oneDirectMatch.hasPlayedAsLocal()
                 && oneDirectMatch.hasPlayedAsVisiting()
-                && otherDirectMatch.hasPlayedAsLocal()        // redundante
+                && otherDirectMatch.hasPlayedAsLocal() // redundante
                 && otherDirectMatch.hasPlayedAsVisiting()) {  // redundante
                 // ordenado por puntos de mayor a menor
                 i = otherDirectMatch.getTotalPoints().compareTo(
                     oneDirectMatch.getTotalPoints());
+
+                /*
+                logger.info("otherDirectMatch->points("
+                    + otherDirectMatch.getTotalPoints()
+                    + ") vs oneDirectMatch->points("
+                    + oneDirectMatch.getTotalPoints()
+                    + ")=>RESULT-i-(" + i + ")"
+                );*/
+
                 if (i != 0) {
                     return i;
                 }
